76a15e2a74585e4f1c43352ca700aa5d84256ce6
bladerunner0001 <bladerunner.pizzaa@gmail.com>
2025-08-26 00:38:45 +0800

    update preloader

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router';
+
+import netflixLogo from "@/assets/logo/Netflix_Logo_RGB.png"
 
 import "./Preloader.scss";
 
 export const Preloader = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timeId = setTimeout(() => {
+            navigate("/browse")
+        }, 2000)
+        return () => clearTimeout(timeId);
+    }, [navigate])
+
     return (
         <div className='cover'>
-            <img id='cover__image' src="" alt="netflix" />
+            <img id='cover__image' src={netflixLogo} alt="netflix" />
             <h1 className='cover__header'>Clone</h1>
         </div>
     )
