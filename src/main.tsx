ffc013fe33181f1a8b9c787865f7f535cd22090c
bladerunner0001 <bladerunner.pizzaa@gmail.com>
2025-08-28 23:12:54 +0800

    refactor banner component and added tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Router } from '@/router/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './scss/style.scss';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<Router />
+		<QueryClientProvider client={queryClient}>
+			<Router />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</StrictMode>
 );
