f2465240fd7ab4ec30e15299a37f322db1baff74
bladerunner0001 <bladerunner.pizzaa@gmail.com>
2025-08-29 17:28:29 +0800

    added movie card list

--- src/components/Movies/List/CaptivatingList/CaptivatingList.tsx
+++ src/components/Movies/List/CaptivatingList/CaptivatingList.tsx
@@ -1,3 +1,16 @@
 export const CaptivatingList = () => {
-	return <div>CaptivatingList</div>;
+	const MOVIES = 40;
+
+	return (
+		<div className="card">
+			<h3 className="card__title">So Completely Captivating</h3>
+			<div className="card__list">
+				{Array.from({ length: MOVIES }, (_, index) => (
+					<div className="card__poster" key={index}>
+						hello
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
